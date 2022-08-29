de6a0de72b6ae1d6e15df18c61138e27276e237d
Sun9802 <80228452+Sun9802@users.noreply.github.com>
2022-08-29 20:36:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import json
 
 today = datetime.now()
+weekList = ["星期一","星期二","星期三","星期四","星期五","星期六","星期七"]
+week = weekList[today.weekday()]
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
@@ -48,6 +51,9 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature, wind = get_weather()
-data = {"diqu":{"value":city, "color":get_random_color()},"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"wind":{"value":wind, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
+data1 = json.dumps(today, default=str, ensure_ascii=False)
+data1 = data1.split('"')
+data2 = data1[1].split(' ')[0]
+data = {"diqu":{"value":city, "color":get_random_color()},"riqi":{"value":data2, "color":get_random_color()},"xingqi":{"value":week, "color":get_random_color()},"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"wind":{"value":wind, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
